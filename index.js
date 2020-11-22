35b0f3a170d2475f2f55e22ab532c31bc39fa1d1
Nan Yang <nanyang@kth.se>
2020-11-22 03:49:26 +0200

    reorganized

--- index.js
+++ index.js
@@ -198,7 +198,7 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: async ({ req }) => {
-    const auth = req?.headers?.authorization;
+    const auth = req.headers.authorization;
     if (auth && auth.toLowerCase().startsWith('bearer ')) {
       const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET);
       const currentUser = await User.findById(decodedToken.id);
